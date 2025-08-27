8c5febd172fcf1f8fbcc91cbb9badc53baeea8c3
Alumno <Alumno@PC45>
2025-08-27 12:14:08 -0300

    Agregar archivos de proyecto.

---
+++ pryEjemploDiFini/Form1.cs
@@ -0,0 +1,15 @@
+namespace pryEjemploDiFini
+{
+    public partial class GitHub : Form
+    {
+        public GitHub()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
